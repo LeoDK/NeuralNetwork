3c61e4b742c5efcffe7adf6b2f550745120201cb
DK <34984338+LeoDK@users.noreply.github.com>
2018-09-01 12:33:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ Needs python-mnist v0.6 and pillow v5.2.0.
 from network import Network, DigitRecognition
 
 #net = DigitRecognition( [[0]*784, [0]*16, [0]*16, [0]*10] )
-net = Network.load("save")
+net = Network.load("save/1")
 net.test(500)
 #net.train(500, 1)
 #net.save("save")
